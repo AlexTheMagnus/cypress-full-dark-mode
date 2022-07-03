fb3e90bba466bf91c98f566fd9c66b470b01a466
AlexTheMagnus <i72lulem@uco.es>
2022-07-03 21:50:23 +0200

    feat: unnecessary pre v10 plugins file

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,10 @@ module.exports = defineConfig({
   viewportHeight: 1200,
   darkMediaQuery: true,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      on('before:spec', (details) => {
+        require('./src')
+      })
     },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
