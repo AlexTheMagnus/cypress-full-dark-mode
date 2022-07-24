484fee3a8b7c45daeb75d9813041725964b43b36
AlexTheMagnus <i72lulem@uco.es>
2022-07-24 20:41:41 +0200

    remove: experiment

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,10 @@
 const { defineConfig } = require('cypress')
-
 module.exports = defineConfig({
   projectId: 'ipnbuo',
   viewportWidth: 1200,
   viewportHeight: 1200,
   darkMediaQuery: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      on('before:spec', (details) => {
-        require('./src')
-      })
-    },
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 })
